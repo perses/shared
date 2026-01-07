ee1b24209b853cec12ee2dabde3fcc129f8040ce
Augustin Husson <husson.augustin@gmail.com>
2026-01-07 13:29:07 +0100

    Release v0.53.0-rc.1 and fix bumping package version (#22)
    Signed-off-by: Augustin Husson <augustin.husson@amadeus.com>

--- scripts/npm-bump/npm-bump.go
+++ scripts/npm-bump/npm-bump.go
@@ -15,6 +15,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
 	"regexp"
 
 	"github.com/perses/perses/scripts/pkg/command"
@@ -23,6 +26,27 @@ import (
 )
 
 var versionPattern = regexp.MustCompile(`^\d+\.\d+\.\d+(?:-[\w\d.]+)?$`)
+
+func updatePackageVersion(workspaces []string, workspacePath string, newVersion string) error {
+	// Use npm version command with --no-git-tag-version to avoid creating git tags
+	// and --allow-same-version to allow setting the same version
+	if err := command.RunInDirectory(workspacePath, "npm", "version", newVersion, "--no-git-tag-version", "--allow-same-version"); err != nil {
+		return err
+	}
+	pkgPath := filepath.Join(workspacePath, "package.json")
+	data, err := os.ReadFile(pkgPath)
+	if err != nil {
+		logrus.WithError(err).Fatalf("unable to read the file %s", pkgPath)
+	}
+	for _, workspace := range workspaces {
+		bumpNPMDeps := regexp.MustCompile(fmt.Sprintf(`"@perses-dev/%s":\s*"(\^)?[0-9]+\.[0-9]+\.[0-9]+(-(alpha|beta|rc)\.[0-9]+)?"`, workspace))
+		data = bumpNPMDeps.ReplaceAll(data, []byte(fmt.Sprintf(`"@perses-dev/%s": "%s"`, workspace, newVersion)))
+	}
+	if writeErr := os.WriteFile(pkgPath, data, 0644); writeErr != nil {
+		logrus.WithError(writeErr).Fatalf("unable to write the file %s", pkgPath)
+	}
+	return nil
+}
 
 func main() {
 	flag.Parse()
@@ -43,27 +67,18 @@ func main() {
 		return
 	}
 
-	if err := updatePackageVersion(".", version); err != nil {
+	if err := updatePackageVersion(workspaces, ".", version); err != nil {
 		logrus.WithError(err).Fatal("failed to update root package.json")
 	}
 
 	logrus.Infof("Updating %d workspace(s) to version %s", len(workspaces), version)
 
 	for _, workspace := range workspaces {
-		if err := updatePackageVersion(workspace, version); err != nil {
+		if err := updatePackageVersion(workspaces, workspace, version); err != nil {
 			logrus.WithError(err).Fatalf("failed to update workspace: %s", workspace)
 		}
 		logrus.Infof("✓ Updated %s to version %s", workspace, version)
 	}
 
 	logrus.Info("All workspace packages updated successfully")
-}
-
-func updatePackageVersion(workspacePath string, newVersion string) error {
-	// Use npm version command with --no-git-tag-version to avoid creating git tags
-	// and --allow-same-version to allow setting the same version
-	if err := command.RunInDirectory(workspacePath, "npm", "version", newVersion, "--no-git-tag-version", "--allow-same-version"); err != nil {
-		return err
-	}
-	return nil
 }
