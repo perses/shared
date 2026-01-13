f756315439f207b15cdf8d4e05c8931bd56fc2e9
Augustin Husson <husson.augustin@gmail.com>
2026-01-13 11:28:18 +0100

    [ignore] Remove copyright date from license headers (#28)
    Signed-off-by: Augustin Husson <augustin.husson@amadeus.com>

--- plugin-system/src/remote/PluginLoaderComponent.tsx
+++ plugin-system/src/remote/PluginLoaderComponent.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-// Copyright 2024 The Perses Authors
+// Copyright The Perses Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
