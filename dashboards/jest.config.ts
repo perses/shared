54ea88ae07ecdd5fc905e2b083b65c23042c83e3
Guillaume LADORME <Gladorme@users.noreply.github.com>
2026-06-08 17:58:30 +0200

    Remove conf headers
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

--- dashboards/jest.config.ts
+++ dashboards/jest.config.ts
@@ -1,6 +1,3 @@
-/**
- * @jest-config-loader ts-node
- */
 // Copyright The Perses Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
