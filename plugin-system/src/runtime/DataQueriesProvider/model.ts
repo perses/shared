114e3d63233ee23383997bf11420ffbdf89e595e
Guillaume LADORME <Gladorme@users.noreply.github.com>
2026-06-11 15:40:08 +0200

    Remove core
    Signed-off-by: Guillaume LADORME <Gladorme@users.noreply.github.com>

--- plugin-system/src/runtime/DataQueriesProvider/model.ts
+++ plugin-system/src/runtime/DataQueriesProvider/model.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { QueryDefinition, UnknownSpec, QueryDataType } from '@perses-dev/core';
+import { QueryDefinition, UnknownSpec, QueryDataType } from '@perses-dev/spec';
 import { QueryObserverOptions, UseQueryResult } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 
