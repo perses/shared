db23fedec1a5112a0fbc5d9ce00a7f8e79b6e57a
Celian GARCIA <celian.garcia@amadeus.com>
2026-08-27 15:55:22 +0200

    Prepare release v0.55.0-beta.5
    Signed-off-by: Celian GARCIA <celian.garcia@amadeus.com>

--- plugin-system/src/remote/PluginRuntime.tsx
+++ plugin-system/src/remote/PluginRuntime.tsx
@@ -100,43 +100,43 @@ const getPluginRuntime = (): ModuleFederation => {
           },
         },
         '@perses-dev/client': {
-          version: '0.55.0-beta.4',
+          version: '0.55.0-beta.5',
           lib: () => require('@perses-dev/client'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.4',
+            requiredVersion: '^0.55.0-beta.5',
           },
         },
         '@perses-dev/components': {
-          version: '0.55.0-beta.4',
+          version: '0.55.0-beta.5',
           lib: () => require('@perses-dev/components'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.4',
+            requiredVersion: '^0.55.0-beta.5',
           },
         },
         '@perses-dev/plugin-system': {
-          version: '0.55.0-beta.4',
+          version: '0.55.0-beta.5',
           lib: () => require('@perses-dev/plugin-system'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.4',
+            requiredVersion: '^0.55.0-beta.5',
           },
         },
         '@perses-dev/explore': {
-          version: '0.55.0-beta.4',
+          version: '0.55.0-beta.5',
           lib: () => require('@perses-dev/explore'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.4',
+            requiredVersion: '^0.55.0-beta.5',
           },
         },
         '@perses-dev/dashboards': {
-          version: '0.55.0-beta.4',
+          version: '0.55.0-beta.5',
           lib: () => require('@perses-dev/dashboards'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.4',
+            requiredVersion: '^0.55.0-beta.5',
           },
         },
         // Below are the shared modules that are used by the plugins, this can be part of the SDK
