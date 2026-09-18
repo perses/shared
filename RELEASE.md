1835664267ae7cdf8dec5833c4b4df7f7609f777
Célian GARCIA <celian.garcia1@gmail.com>
2026-09-18 10:53:15 +0200

    Prepare release v0.55.0-beta.12
    Signed-off-by: Celian GARCIA <celian.garcia@amadeus.com>

--- RELEASE.md
+++ RELEASE.md
@@ -2,12 +2,13 @@
 
 To release a new version of the libraries, you should:
 
-1. Checkout to a new branch named `release-vX.Y.Z` from main.
+1. Checkout to a new branch named `<yourname>/release-v<major>.<minor>.<patch>` from main.
 2. Bump the version of the libraries running [npm-bump.go](./scripts/npm-bump/npm-bump.go) `go run ./scripts/npm-bump <new version>`. For example: `go run ./scripts/npm-bump 1.2.3`.
 3. Run `npm install`
-4. Commit these changes - as a standalone commit ("Prepare release vX.Y.Z") or as part of your changes.
-5. Push the changes (new version(s)) and create a PR.
-6. After the PR is merged, checkout to and update the main.
-7. Run [release.go](./scripts/release/release.go) `go run ./scripts/release`.
+4. [not yet automatized] Update the version manually into ``plugin-system/src/remote/PluginRuntime.tsx`` 
+5. Commit these changes - as a standalone commit ("Prepare release vX.Y.Z") or as part of your changes. 
+6. Push the changes (new version(s)) and create a PR. 
+7. After the PR is merged, checkout to and update the main. 
+8. Run [release.go](./scripts/release/release.go) `go run ./scripts/release`.
 
 Further actions will then be triggered on GitHub side (see release stage in the [CI](./.github/workflows/ci.yml)).
--- plugin-system/src/remote/PluginRuntime.tsx
+++ plugin-system/src/remote/PluginRuntime.tsx
@@ -137,43 +137,43 @@ const getPluginRuntime = (): ModuleFederation => {
           },
         },
         '@perses-dev/client': {
-          version: '0.55.0-beta.11',
+          version: '0.55.0-beta.12',
           lib: () => getHostSharedModule('@perses-dev/client'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.11',
+            requiredVersion: '^0.55.0-beta.12',
           },
         },
         '@perses-dev/components': {
-          version: '0.55.0-beta.11',
+          version: '0.55.0-beta.12',
           lib: () => getHostSharedModule('@perses-dev/components'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.11',
+            requiredVersion: '^0.55.0-beta.12',
           },
         },
         '@perses-dev/plugin-system': {
-          version: '0.55.0-beta.11',
+          version: '0.55.0-beta.12',
           lib: () => getHostSharedModule('@perses-dev/plugin-system'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.11',
+            requiredVersion: '^0.55.0-beta.12',
           },
         },
         '@perses-dev/explore': {
-          version: '0.55.0-beta.11',
+          version: '0.55.0-beta.12',
           lib: () => getHostSharedModule('@perses-dev/explore'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.11',
+            requiredVersion: '^0.55.0-beta.12',
           },
         },
         '@perses-dev/dashboards': {
-          version: '0.55.0-beta.11',
+          version: '0.55.0-beta.12',
           lib: () => getHostSharedModule('@perses-dev/dashboards'),
           shareConfig: {
             singleton: true,
-            requiredVersion: '^0.55.0-beta.11',
+            requiredVersion: '^0.55.0-beta.12',
           },
         },
         // Below are the shared modules that are used by the plugins and are loaded asynchronously on demand using get rather than lib.
